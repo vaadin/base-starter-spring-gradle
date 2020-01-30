412b2dfb0b8968031c2b1575f545c03a4f702e67
Martin Vysny <mavi@vaadin.com>
2020-01-30 08:49:26 +0200

    Initial import

---
+++ src/main/java/com/example/demo/GreetService.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
