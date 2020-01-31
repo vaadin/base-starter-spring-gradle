ceba2afcce15e24452eb3d93c2938d069c13d360
Matti Tahvonen <matti@vaadin.com>
2020-01-31 20:59:19 +0200

    Added travis config and smoke test script

---
+++ smoke-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -ev
+
+echo Trying out development mode
+
+./gradlew clean vaadinPrepareFrontend
+
+if grep -q '"productionMode": false' build/vaadin-generated/META-INF/VAADIN/config/flow-build-info.json; then
+    echo Development mode is properly on
+else
+	echo "Production mode is off although in development mode" >&2
+	exit 1
+fi
+
+echo Building production war file
+
+./gradlew clean vaadinBuildFrontend build
+
+if grep -q '"productionMode": true' build/vaadin-generated/META-INF/VAADIN/config/flow-build-info.json; then
+  echo Production mode is on
+else
+	echo "Production mode is off although in development mode" >&2
+	exit 1
+fi
+
+if test -f build/libs/base-starter-spring-gradle-0.0.1-SNAPSHOT.jar; then
+	echo "JAR file exists"
+else
+	echo "JAR file not built!" >&2
+	exit 1
+fi
+
+# TODO run web app and check that 200 is returned from root
+
+echo "Smoke test passed"
+
+### 
