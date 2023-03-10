8fe29271cb3d8eaa47ad0006880521db4d8d5ec7
Matti Tahvonen <matti@vaadin.com>
2023-03-10 15:08:58 +0200

    Using Paragraph instead of Notification
    Helps new users to better figure out essence of Vaadin

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -23,8 +23,9 @@ public class MainView extends VerticalLayout {
 
         // Button click listeners can be defined as lambda expressions
         GreetService greetService = new GreetService();
-        Button button = new Button("Say hello",
-                e -> Notification.show(greetService.greet(textField.getValue())));
+        Button button = new Button("Say hello", e ->  {
+            add(new Paragraph(greetService.greet(textField.getValue())));
+        });
 
         // Theme variants give you predefined extra styles for components.
         // Example: Primary button is more prominent look.
