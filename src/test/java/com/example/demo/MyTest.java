3bb67b46700c7da3b9fd14f1e7240d3eca3c9e93
Marco Collovati <mcollovati@gmail.com>
2025-12-01 10:12:09 +0100

    test: add commercial add-on

---
+++ src/test/java/com/example/demo/MyTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.vaadin.testbench.BrowserTest;
+import com.vaadin.testbench.BrowserTestBase;
+
+public class MyTest extends BrowserTestBase {
+
+    @BrowserTest
+    void testME() {
+
+    }
+}
